d2b222abf9eac8ee0520fc2f1acfce0e9bdc3c4a
Jonny Pillar <info@jonnypillar.co.uk>
2020-09-27 20:59:15 +0100

    Fixes

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment, incrementByAmount, incrementAsync, selectCount } from './counterSlice'
+import {
+    decrement,
+    increment,
+    incrementByAmount,
+    incrementAsync,
+    selectCount,
+} from './counterSlice'
 import styles from './Counter.module.css'
 
 export function Counter() {
@@ -11,11 +17,19 @@ export function Counter() {
     return (
         <div>
             <div className={styles.row}>
-                <button className={styles.button} aria-label="Increment value" onClick={() => dispatch(increment())}>
+                <button
+                    className={styles.button}
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
                     +
                 </button>
                 <span className={styles.value}>{count}</span>
-                <button className={styles.button} aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+                <button
+                    className={styles.button}
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
                     -
                 </button>
             </div>
