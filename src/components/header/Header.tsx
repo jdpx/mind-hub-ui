1a688907f81eea6e6a4a48d55c108330870f8237
Jonny Pillar <12187067+JonnyPillar@users.noreply.github.com>
2020-09-27 18:55:15 +0100

    Added SCSS Compilation (#6)
    * Adding Scss
    
    * Added Logo

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../logo/Logo'
+
+import './Header.scss'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <Logo />
+        </div>
+    )
+}
---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import logo from '../../assets/mind-logo.png'
+
+export default function Logo() {
+    return <img src={logo} alt="Mind In Salford Logo" />
+}
