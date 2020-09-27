4bc064b6dd8da1114f7b6b72afa5970cb48e3be7
Jonny Pillar <info@jonnypillar.co.uk>
2020-09-27 18:46:36 +0100

    Adding Scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+import * as serviceWorker from './serviceWorker'
 
-import { BrowserRouter as Router } from "react-router-dom";
-import Auth0ProviderWithHistory from './auth0/auth0-provider-with-history';
+import { BrowserRouter as Router } from 'react-router-dom'
+import Auth0ProviderWithHistory from './auth0/auth0-provider-with-history'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Auth0ProviderWithHistory>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </Auth0ProviderWithHistory>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Router>
+            <Auth0ProviderWithHistory>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </Auth0ProviderWithHistory>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
