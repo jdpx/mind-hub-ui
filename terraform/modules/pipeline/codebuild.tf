6b265ccc38b4461c2a40883d1f511363cafdf8b6
Jonny Pillar <12187067+JonnyPillar@users.noreply.github.com>
2020-09-27 16:45:25 +0100

    Split Terraform and UI Deploy PIpeline (#4)

--- terraform/modules/pipeline/codebuild.tf
+++ terraform/modules/pipeline/codebuild.tf
@@ -1,16 +1,13 @@
 resource "aws_codebuild_project" "mind_hub_ui_build" {
-  name           = "mind-hub-ui-build-${var.env}"
+  name           = "mind-hub-ui-build"
   badge_enabled  = false
   build_timeout  = 60
   queued_timeout = 480
   service_role   = aws_iam_role.mind_hub_ui_build_role.arn
-  tags = {
-    Environment = var.env
-  }
 
   artifacts {
     encryption_disabled    = false
-    name                   = "mind_hub_ui_build-${var.env}"
+    name                   = "mind_hub_ui_build_artifact"
     override_artifact_name = false
     packaging              = "NONE"
     type                   = "CODEPIPELINE"
@@ -36,9 +33,47 @@ resource "aws_codebuild_project" "mind_hub_ui_build" {
   }
 
   source {
-    type      = "CODEPIPELINE"
-    buildspec = "ci/buildspec.yml"
-    # buildspec_file_path = var.buildspec_file_path
+    type                = "CODEPIPELINE"
+    buildspec           = "ci/pipeline_build_buildspec.yml"
+    git_clone_depth     = 0
+    insecure_ssl        = false
+    report_build_status = false
+  }
+}
+
+resource "aws_codebuild_project" "mind_hub_terraform_deploy" {
+  name           = "mind_hub_terraform_deploy"
+  badge_enabled  = false
+  build_timeout  = 60
+  queued_timeout = 480
+  service_role   = aws_iam_role.mind_hub_terraform_deploy_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:2.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = false
+    type                        = "LINUX_CONTAINER"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      status = "ENABLED"
+    }
+
+    s3_logs {
+      encryption_disabled = false
+      status              = "DISABLED"
+    }
+  }
+
+  source {
+    type                = "CODEPIPELINE"
+    buildspec           = "ci/pipeline_terraform_deploy_buildspec.yml"
     git_clone_depth     = 0
     insecure_ssl        = false
     report_build_status = false
