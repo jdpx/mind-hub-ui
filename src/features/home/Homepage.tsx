fe5f5e83ffb59beaddcc76da946d9bb757d9ff90
Jonny Pillar <info@jonnypillar.co.uk>
2020-10-01 16:34:35 +0100

    Added WebFonts

--- src/features/home/Homepage.tsx
+++ src/features/home/Homepage.tsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
+
+import logo from '../../assets/home-welcome.svg'
+import './Homepage.scss'
 
 export default class Homepage extends Component {
     render() {
-        return <div />
+        return (
+            <div className="home-page" data-testid="home-page">
+                <Container>
+                    <div className="d-flex flex-column justify-content-center">
+                        <h1>Welcome to Mind E-Learning</h1>
+                        <img src={logo} alt="Mind In Salford Logo" />
+                    </div>
+                </Container>
+            </div>
+        )
     }
 }
