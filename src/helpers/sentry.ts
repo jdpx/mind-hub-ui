53a64c336d782259b4472d16e052e5d6a231b1c5
Jonny Pillar <12187067+JonnyPillar@users.noreply.github.com>
2020-10-12 12:35:00 +0100

    Added Release To Sentry (#25)

--- src/helpers/sentry.ts
+++ src/helpers/sentry.ts
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/react'
 import { Integrations } from '@sentry/tracing'
 
 const sentryDSN = process.env.REACT_APP_SENTRY_DSN || ''
+const releaseNumber = process.env.provREACT_APP_BUILD_NUMBER || ''
 
 export function init() {
     if (process.env.NODE_ENV === 'development') {
@@ -11,7 +12,7 @@ export function init() {
     Sentry.init({
         dsn: sentryDSN,
         integrations: [new Integrations.BrowserTracing()],
-
+        release: releaseNumber,
         tracesSampleRate: 1.0,
     })
 }
