1207c92b833973f8f2cbbd95dfea6f708fe54954
Jonny Pillar <info@jonnypillar.co.uk>
2020-11-16 18:21:18 +0000

    Added Not Found Route

--- src/features/notFound/NotFoundPage.tsx
+++ src/features/notFound/NotFoundPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Page from '../../components/Page/Page'
+import NotFound from './NotFound'
 
 export default function NotFoundPage() {
     return (
         <Page name="not-found">
-            <div>Not Found</div>
+            <NotFound />
         </Page>
     )
 }
