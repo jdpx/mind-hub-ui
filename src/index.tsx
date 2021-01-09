65f1a348c5c0b8d6c3461a4c3872904cdb4d6f9b
Jonny Pillar <info@jonnypillar.co.uk>
2021-01-09 19:16:13 +0000

    Remove JSDOM From Tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import './index.scss'
 import App from './App'
-import { store } from './app/store'
 import * as serviceWorker from './serviceWorker'
 
 import Auth0ProviderWithHistory from './providers/AuthenticationProvider'
@@ -23,9 +20,7 @@ ReactDOM.render(
         <Router>
             <Auth0ProviderWithHistory>
                 <AuthorizedApolloProvider>
-                    <Provider store={store}>
-                        <App />
-                    </Provider>
+                    <App />
                 </AuthorizedApolloProvider>
             </Auth0ProviderWithHistory>
         </Router>
